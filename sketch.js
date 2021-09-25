7137301050c746e8820113ac2ee41b2bbcf7a48b
SHolmes123 <83624773+SHolmes123@users.noreply.github.com>
2021-09-25 16:12:27 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,50,50)
+}
+
+function draw() 
+{
+  background("white");
+  if (keyDown("right")) {
+    box.x = box.x + 5
+  }
+  if (keyDown("left")) {
+    box.x =  box.x - 5
+  }
+  if (keyDown("up")){
+    box.y = box.y - 5 
+  }
+  if (keyDown("down")) {
+    box.y = box.y + 5
+  }
+drawSprites()
+}
+
+
+
+
